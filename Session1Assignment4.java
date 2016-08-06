c372cb75b9943c33a6af7a95e29915bef523793b
ashishbansal1975 <ashish_bansal01@yahoo.com>
2016-08-06 17:46:03 +0530

    Hi

---
+++ Session1Assignment4.java
@@ -0,0 +1,19 @@
+public class Session1Assignment4
+{
+    public static void main(String [] args)
+    {
+            int x,y;
+            x =5;
+            y = 10;     
+                         
+            System.out.println(x+y*2);            
+            System.out.println(x-(y+2));// x-y+2 is equal to x-(y+2): BODMAS rule
+            System.out.println((x+y)*2);            
+            System.out.println(y%x);
+
+
+
+
+     }
+
+}
